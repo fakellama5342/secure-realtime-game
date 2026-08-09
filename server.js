104c767a582ae629c7685873be0758731b23899c
steward castillo <104889067+fakellama5342@users.noreply.github.com>
2026-08-09 17:35:47 -0600

    Update import paths for connection and testing files

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
+
 require('dotenv').config();
 const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const myDB = require('../connection');
+const fccTesting = require('../freeCodeCamp/fcctesting.js');
 const helmet = require('helmet');
 
 const app = express();
@@ -12,14 +13,15 @@ app.use(helmet.xssFilter());
 app.use(helmet.noCache());
 app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-
-});
+app.route('/')
+  .get((req, res) => {
+    
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
