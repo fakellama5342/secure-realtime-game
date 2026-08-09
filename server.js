17d92224f3b5595b4c1e5616ef0b91e7bb0eeadc
steward castillo <104889067+fakellama5342@users.noreply.github.com>
2026-08-09 17:49:48 -0600

    Refactor helmet middleware usage in server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const helmet = require('helmet');
 
 const app = express();
 
-app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
 app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
 app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'"] } }));
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.4.3' }));
 
 fccTesting(app); // For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
